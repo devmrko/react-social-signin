66a00893892a4d38204ca7949ade3e6b5289c604
devmrko <devmrko@gmail.com>
2025-01-07 15:48:59 +0900

    Update App.js
    remove calling env separately because it already called by react, and it causes the error

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -31,6 +5,7 @@ import Header from './components/Header';
 import Navigation from './components/Navigation';
 import RoutesComponent from './components/RoutesComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   console.log('Google Client ID:', process.env.REACT_APP_GOOGLE_CLIENT_ID); // Debug log

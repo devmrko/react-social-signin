c836d72a6e94f4455da0b7fac21f5a5905cfef54
devmrko <devmrko@gmail.com>
2025-01-14 11:15:32 +0900

    add env export shell script

---
+++ apply_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "# apply env"
+
+export GOOGLE_CLIENT_ID=
+export API_BASE_URL=
+export YOUTUBE_API_KEY=
+
+echo 'GOOGLE_CLIENT_ID: ' $GOOGLE_CLIENT_ID
+echo 'API_BASE_URL: ' $API_BASE_URL
+echo 'YOUTUBE_API_KEY: ' $YOUTUBE_API_KEY
+
